6a27ee58aaaffac3151e7c0091bd9fa912db5a6f
Micah DeBruno <32474022+micahdebruno@users.noreply.github.com>
2021-05-24 09:30:13 -0700

    Login and signup layout

---
+++ app/src/main/java/auth/SignUp.java
@@ -0,0 +1,16 @@
+package auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myblog.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
